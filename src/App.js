55fa42bcb3c26d4b460009cac2cf6c27b880cf41
Thanos <52926743+JovanJs@users.noreply.github.com>
2019-07-15 22:36:22 +0100

    connected netlify

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
     <Layout>
         <Header className="header-color" title="JS Engineer" scroll>
             <Navigation>
+                <Link to="/">Home</Link>
                 <Link to="/resume">Resume</Link>
                 <Link to="/aboutme">About Me</Link>
                 <Link to="/projects">Projects</Link>
